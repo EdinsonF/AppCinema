7c8d3526e0090ebbd5796fe7179be3b04a57573c
Yisus <desarrollador@kubbox.com>
2019-01-10 14:35:26 -0500

    configuracion del config/database Laravel como standar

--- config/database.php
+++ config/database.php
@@ -53,16 +53,16 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'database' => 'cinema',
-            'username' => 'root',
-            'password' =>  '',
-            'charset' => 'utf8',
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => false,
-            'engine' => null,
+            'prefix'    => '',
+            'strict'    => false,
+            'engine'    => null,
         ],
 
         'pgsql' => [
